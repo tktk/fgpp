dc9fbda1b66ce92d5905575fff9169f18099d010
tekuto <g.tekuto@gmail.com>
2014-02-24 17:23:27 +0900

    文字列型追加
    以下のテンプレートクラスを追加
    ・fg::StringViewTmpl
    以下の型を追加
    ・fg::StringView
    ・fg::Utf8View
    ・fg::Utf16View
    ・fg::Utf32View
    ・fg::String
    ・fg::Utf8
    ・fg::Utf16
    ・fg::Utf32

--- inc/fgpp/def/common/primitives.h
+++ inc/fgpp/def/common/primitives.h
@@ -2,6 +2,7 @@
 #define FGPP_DEF_COMMON_PRIMITIVES_H
 
 #include <cstddef>
+#include <string>
 
 namespace fg {
     typedef bool Bool;
@@ -24,6 +25,93 @@ namespace fg {
     typedef char Utf8Char;
     typedef char16_t Utf16Char;
     typedef char32_t Utf32Char;
+
+    template< typename CHAR_T >
+    class StringViewTmpl
+    {
+    private:
+        CHAR_T *    ptr;
+        Size        length;
+
+    public:
+        StringViewTmpl(
+        ) = default;
+
+        StringViewTmpl(
+            decltype( ptr )         _ptr
+            , decltype( length )    _length
+        )
+            : ptr( _ptr )
+            , length( _length )
+        {
+        }
+
+        StringViewTmpl(
+            std::basic_string< CHAR_T > &   _string
+        )
+            : StringViewTmpl(
+                const_cast< CHAR_T * >( _string.data() )
+                , _string.length()
+            )
+        {
+        }
+
+        decltype( length ) getLength(
+        ) const
+        {
+            return this->length;
+        }
+
+        void setLength(
+            decltype( length )  _length
+        )
+        {
+            this->length = _length;
+        }
+
+        const decltype( *ptr ) & operator[](
+            decltype( length )  _index
+        ) const
+        {
+            return this->ptr[ _index ];
+        }
+
+        decltype( *ptr ) & operator[](
+            decltype( length )  _index
+        )
+        {
+            return this->ptr[ _index ];
+        }
+
+        void assign(
+            decltype( ptr )         _ptr
+            , decltype( length )    _length
+        )
+        {
+            this->ptr = _ptr;
+            this->length = _length;
+        }
+
+        void assign(
+            std::basic_string< CHAR_T > &   _string
+        )
+        {
+            this->assign(
+                const_cast< CHAR_T * >( _string.data() )
+                , _string.length()
+            );
+        }
+    };
+
+    typedef StringViewTmpl< StringChar > StringView;
+    typedef StringViewTmpl< Utf8Char > Utf8View;
+    typedef StringViewTmpl< Utf16Char > Utf16View;
+    typedef StringViewTmpl< Utf32Char > Utf32View;
+
+    typedef std::basic_string< StringChar > String;
+    typedef std::basic_string< Utf8Char > Utf8;
+    typedef std::basic_string< Utf16Char > Utf16;
+    typedef std::basic_string< Utf32Char > Utf32;
 }
 
 #endif  // FGPP_DEF_COMMON_PRIMITIVES_H
